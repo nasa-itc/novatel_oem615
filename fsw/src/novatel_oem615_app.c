c3fbb7fc05f25fbe7d3da3ec154d5d70383839d3
Lucas, John P <John.P.Lucas@nasa.gov>
2024-03-04 19:15:33 -0500

    [nasa/nos3#226] Updates to build 64-bit fsw;

--- fsw/src/novatel_oem615_app.c
+++ fsw/src/novatel_oem615_app.c
@@ -585,7 +585,7 @@ int32 NOVATEL_OEM615_VerifyCmdLength(CFE_MSG_Message_t * msg, uint16 expected_le
         CFE_MSG_GetFcnCode(msg, &cmd_code);
 
         CFE_EVS_SendEvent(NOVATEL_OEM615_LEN_ERR_EID, CFE_EVS_EventType_ERROR,
-           "Invalid msg length: ID = 0x%X,  CC = %d, Len = %d, Expected = %d",
+           "Invalid msg length: ID = 0x%X,  CC = %d, Len = %ld, Expected = %d",
               CFE_SB_MsgIdToValue(msg_id), cmd_code, actual_length, expected_length);
 
         status = OS_ERROR;
