dd4838b3ef339220590bc781f64a48585bc7bdfd
dacarter22 <davidacarter22@gmail.com>
2024-12-19 12:47:28 -0500

    [nasa/nos3#383] Added files generated from sample script adapted to novatel_oem615

---
+++ fsw/standalone/device_cfg.h
@@ -0,0 +1,15 @@
+#ifndef _NOVATEL_OEM615_CHECKOUT_DEVICE_CFG_H_
+#define _NOVATEL_OEM615_CHECKOUT_DEVICE_CFG_H_
+
+/*
+** NOVATEL_OEM615 Checkout Configuration
+*/
+#define NOVATEL_OEM615_CFG
+/* Note: NOS3 uart requires matching handle and bus number */
+#define NOVATEL_OEM615_CFG_STRING           "/dev/usart_16"
+#define NOVATEL_OEM615_CFG_HANDLE           16 
+#define NOVATEL_OEM615_CFG_BAUDRATE_HZ      115200
+#define NOVATEL_OEM615_CFG_MS_TIMEOUT       250
+#define NOVATEL_OEM615_CFG_DEBUG
+
+#endif /* _NOVATEL_OEM615_CHECKOUT_DEVICE_CFG_H_ */
