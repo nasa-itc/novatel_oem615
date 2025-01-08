ff917855b4ce8c6a3a161806de2eda4a1fe6e165
Kevin Bruce <kevin.c.bruce@nasa.gov>
2025-01-08 14:48:34 -0500

    [nasa/nos3#383] added a gitignore, and minor modifications to checkout

--- fsw/standalone/novatel_oem615_checkout.c
+++ fsw/standalone/novatel_oem615_checkout.c
@@ -147,31 +147,30 @@ int process_command(int cc, int num_tokens, char tokens[MAX_INPUT_TOKENS][MAX_IN
                 {
                     //uint8_t read_data[NOVATEL_OEM615_DEVICE_DATA_SIZE];
                     OS_printf("NOVATEL_OEM615_RequestData command success\n");
-                    /*
                     // TODO - Fix print statements, error says Novatel_oem615Data has no DeviceCounter attribute but its declaration is similar to HK so not sure why
-                    // OS_printf("NovatelData.DeviceCounter = %d \n", Novatel_oem615Data.DeviceDataZ);
-                    // OS_printf("NovatelData.DeviceDataX = %d \n", Novatel_oem615Data.DeviceDataX);
-                    // OS_printf("NovatelData.DeviceDataY = %d \n", Novatel_oem615Data.DeviceDataY);
-                    // OS_printf("NovatelData.DeviceDataZ = %d \n", Novatel_oem615Data.DeviceDataZ);
-                    if ((read_data[0]  == NOVATEL_OEM615_DEVICE_HDR_0)     && 
-                        (read_data[1]  == NOVATEL_OEM615_DEVICE_HDR_1)     && 
-                        (read_data[12] == NOVATEL_OEM615_DEVICE_TRAILER_0) && 
-                        (read_data[13] == NOVATEL_OEM615_DEVICE_TRAILER_1) )
-                    {
-                        Novatel_oem615Data.DeviceCounter  = read_data[2] << 24;
-                        Novatel_oem615Data.DeviceCounter |= read_data[3] << 16;
-                        Novatel_oem615Data.DeviceCounter |= read_data[4] << 8;
-                        Novatel_oem615Data.DeviceCounter |= read_data[5];
+                    OS_printf("NovatelData.DeviceCounter = %d \n", Novatel_oem615Data.DeviceCounter);
+                    OS_printf("NovatelData.DeviceDataX = %d \n", Novatel_oem615Data.DeviceDataX);
+                    OS_printf("NovatelData.DeviceDataY = %d \n", Novatel_oem615Data.DeviceDataY);
+                    OS_printf("NovatelData.DeviceDataZ = %d \n", Novatel_oem615Data.DeviceDataZ);
+                    // if ((read_data[0]  == NOVATEL_OEM615_DEVICE_HDR_0)     && 
+                    //     (read_data[1]  == NOVATEL_OEM615_DEVICE_HDR_1)     && 
+                    //     (read_data[12] == NOVATEL_OEM615_DEVICE_TRAILER_0) && 
+                    //     (read_data[13] == NOVATEL_OEM615_DEVICE_TRAILER_1) )
+                    // {
+                    //     Novatel_oem615Data.DeviceCounter  = read_data[2] << 24;
+                    //     Novatel_oem615Data.DeviceCounter |= read_data[3] << 16;
+                    //     Novatel_oem615Data.DeviceCounter |= read_data[4] << 8;
+                    //     Novatel_oem615Data.DeviceCounter |= read_data[5];
 
-                        Novatel_oem615Data.DeviceDataX  = read_data[6] << 8;
-                        Novatel_oem615Data.DeviceDataX |= read_data[7];
+                    //     Novatel_oem615Data.DeviceDataX  = read_data[6] << 8;
+                    //     Novatel_oem615Data.DeviceDataX |= read_data[7];
 
-                        Novatel_oem615Data.DeviceDataY  = read_data[8] << 8;
-                        Novatel_oem615Data.DeviceDataY |= read_data[9];
+                    //     Novatel_oem615Data.DeviceDataY  = read_data[8] << 8;
+                    //     Novatel_oem615Data.DeviceDataY |= read_data[9];
                         
-                        Novatel_oem615Data.DeviceDataZ  = read_data[10] << 8;
-                        Novatel_oem615Data.DeviceDataZ |= read_data[11];
-                    }*/
+                    //     Novatel_oem615Data.DeviceDataZ  = read_data[10] << 8;
+                    //     Novatel_oem615Data.DeviceDataZ |= read_data[11];
+                    // }
                 }
                 else
                 {
@@ -234,6 +233,11 @@ int main(int argc, char *argv[])
     Novatel_oem615HK.DeviceCounter = 0;
     Novatel_oem615HK.DeviceConfig = 0;
     Novatel_oem615HK.DeviceStatus = 0;
+
+    Novatel_oem615Data.DeviceCounter = 0;
+    Novatel_oem615Data.DeviceDataX = 0;
+    Novatel_oem615Data.DeviceDataY = 0;
+    Novatel_oem615Data.DeviceDataZ = 0;
 
 
     status = uart_init_port(&Novatel_oem615Uart);
